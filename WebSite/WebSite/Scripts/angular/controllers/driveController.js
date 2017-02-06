46a753d98d233cfec16336588aaa95b59aa15f5a
ZheldakArtem <artemzheldak1995@gmail.com>
2017-02-06 19:13:46 +0300

    added navbar-row

--- WebSite/WebSite/Scripts/angular/controllers/driveController.js
+++ WebSite/WebSite/Scripts/angular/controllers/driveController.js
@@ -1,98 +1,102 @@
-﻿
-angular.module('main', ['systemServiceModule', 'underscoreService'])
-.controller('driveController', [
-    '$scope', 'systemService', '_',
-function ($scope, service, _) {
+﻿(function () {
+    angular.module('main', ['systemServiceModule', 'underscoreService'])
+    .controller('driveController', [
+        '$scope', 'systemService', '_',
+    function ($scope, service, _) {
 
-    $scope.drives = [];
+        $scope.arrFullPath = [];
+        $scope.drives = [];
 
-    $scope.getSubItems = function (path) {
-        $scope.currentPath = path;
-        service.getFiles(path).then(function (response) {
-            $scope.Files = [];
+        $scope.getSubItems = function (path) {
+            $scope.arrFullPath.push(path);
+            $scope.currentPath = path;
+            service.getFiles(path).then(function (response) {
+                $scope.Files = [];
+                response.data.forEach(function (element, index, array) {
+                    $scope.Files.push(element);
+                });
+            });
+
+            service.getFolders(path).then(function (response) {
+                $scope.Folders = [];
+                response.data.forEach(function (element, index, array) {
+                    $scope.Folders.push(element);
+                });
+            }, function () {
+                alert("Not access!!!");
+            });
+        }
+
+        $scope.deleteFolder = function (path, index) {
+            if (!confirm("Are you shure?")) {
+                return;
+            }
+
+            $scope.Folders.splice(index, 1);
+
+            service.deleteFolder(path).then(function () {
+                alert("success!!!");
+            }, function () {
+                alert("Not access!!!");
+            });
+        }
+
+        $scope.deleteFile = function (path, index) {
+            if (!confirm("Are you shure?")) {
+                return;
+            }
+
+            $scope.Files.splice(index, 1);
+
+            service.deleteFile(path).then(function () {
+                alert("success!!!");
+            }, function () {
+                alert("Not access!!!");
+            });
+        }
+
+        $scope.createItem = function (name) {
+            if (typeof (name) === "undefined" || typeof (name) === null) {
+                alert('The path is empty.');
+                return;
+            }
+            if ($scope.createType === "folder") {
+                service.createFolder({ Name: name, Path: $scope.currentPath }).then(function (result) {
+                    var temp = result;
+                    $scope.Folders.push($scope.currentPath + name);
+                },
+                function () {
+                    alert('Creation folder error.');
+                });
+            } else {
+                service.createFile({ Name: name, Path: $scope.currentPath }).then(function () {
+                    $scope.Files.push($scope.currentPath + name);
+                },
+                function () {
+                    alert('Creation file error.');
+                });
+            }
+        }
+
+        service.getDrives().then(function (response) {
             response.data.forEach(function (element, index, array) {
-                $scope.Files.push(element);
+                $scope.drives.push(element);
             });
         });
 
-        service.getFolders(path).then(function (response) {
-            $scope.Folders = [];
-            response.data.forEach(function (element, index, array) {
-                $scope.Folders.push(element);
-            });
-        }, function () {
-            alert("Not access!!!");
-        });
-    }
+        $scope.driveNamesFilter = function (arrayOfNames) {
 
-    $scope.deleteFolder = function (path, index) {
-        if (!confirm("Are you shure?")) {
-            return;
+            var result = [];
+            for (var i = 0; i < arrayOfNames.length; i++) {
+                result.push(arrayOfNames[i].charAt(0));
+            }
+            return _.first(result);
         }
 
-        $scope.Folders.splice(index, 1);
+        $scope.elementNamesFilter = function (elemName) {
+            var result = _.last(elemName.split('\\'));
 
-        service.deleteFolder(path).then(function () {
-            alert("success!!!");
-        }, function () {
-            alert("Not access!!!");
-        });
-    }
-
-    $scope.deleteFile = function (path, index) {
-        if (!confirm("Are you shure?")) {
-            return;
+            return result === "" ? elemName.charAt(0) : result;
         }
-
-        $scope.Files.splice(index, 1);
-
-        service.deleteFile(path).then(function () {
-            alert("success!!!");
-        }, function () {
-            alert("Not access!!!");
-        });
-    }
-
-    $scope.createItem = function (name) {
-        if (typeof (name) === "undefined" || typeof (name) === null) {
-            alert('The path is empty.');
-            return;
-        }
-        if ($scope.createType === "folder") {
-            service.createFolder({ Name: name, Path: $scope.currentPath }).then(function (result) {
-                var temp = result;
-                $scope.Folders.push($scope.currentPath + name);
-            },
-            function () {
-                alert('Creation folder error.');
-            });
-        } else {
-            service.createFile({ Name: name, Path: $scope.currentPath }).then(function () {
-                $scope.Files.push($scope.currentPath + name);
-            },
-            function () {
-                alert('Creation file error.');
-            });
-        }
-    }
-
-
-    service.getDrives().then(function (response) {
-        response.data.forEach(function (element, index, array) {
-            $scope.drives.push(element);
-        });
-    });
-
-    $scope.driveNamesFilter = function (arrayOfNames) {
-
-        var result = [];
-        for (var i = 0; i < arrayOfNames.length; i++) {
-            result.push(arrayOfNames[i].charAt(0));
-        }
-        return _.first(result);
-    }
-
-    $scope.ElementsNameFilter = function (elemName) {
-        return _.last(elemName.split('\\'));
-    }
-}]);
+    }]);
+}())
